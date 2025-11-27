02bbeac205e56da36d7b4d7f3dbecca9a6f632c3
518H0090 <nhoxhieuro5@gmail.com>
2025-11-27 16:23:40 +0700

    Extension Method

---
+++ dotnet/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ExtensionMethod
+{
+    public static class IntExtension
+    {
+        public static int SumAnotherTwoNumbers(this int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static void Print(this string text, ConsoleColor color = ConsoleColor.Red)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 20;
+            
+            Console.WriteLine(num1.SumAnotherTwoNumbers(30, 60));
+
+            string text = "Test String Extension";
+
+            text.Print(ConsoleColor.DarkBlue);
+
+            Console.WriteLine(text);
+
+            Console.ReadKey();
+        }
+    }
+}
