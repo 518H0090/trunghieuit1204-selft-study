88f529f6037f9ef9c8e21b639aa0b327177da2c0
518H0090 <nhoxhieuro5@gmail.com>
2025-11-28 14:00:52 +0700

    Basic Javascript

---
+++ javascript/read_document/helloworld/helloworld.js
@@ -0,0 +1,21 @@
+alert("Hello World That Good Javascript");
+
+let message;
+
+console.log(message);
+
+message = "Hello World";
+
+console.log(message);
+
+message = 10;
+
+console.log(message);
+
+console.log(counter);
+
+const workday = 5;
+
+console.log(workday);
+
+workday = 10;
