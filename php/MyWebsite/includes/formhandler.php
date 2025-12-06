627620c70e179100015cb4331aeee67b3a8d3aed
518H0090 <nhoxhieuro5@gmail.com>
2025-12-06 23:02:00 +0700

    php basic

---
+++ php/MyWebsite/includes/formhandler.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = htmlspecialchars($_POST["firstname"]);
+    $lastname = htmlspecialchars($_POST["lastname"]);
+    $pets = htmlspecialchars($_POST["favouritepet"]);
+
+    if (empty($firstname)) {
+        exit();
+        header("Location: ../index.php");
+    }
+
+    echo "These are the data, that the user submitted: ";
+    echo "<br />";
+    echo $firstname;
+    echo "<br />";
+    echo $lastname;
+    echo "<br />";
+    echo $pets;
+}
